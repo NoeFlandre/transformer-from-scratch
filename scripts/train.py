fbb2f57abfd02e4ba812878a51d9e32e758a56ab
NoeFlandre <noeflandre@gmail.com>
2026-02-12 17:12:39 +0530

    get or build tokenizer

---
+++ scripts/train.py
@@ -0,0 +1,20 @@
+
+from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.trainers import WordLevelTrainer
+from tokenizers.pre_tokenizers import Whitespace
+
+from pathlib import Path
+
+def get_or_build_tokenizer(config, dataset, language):
+    tokenizer_path = Path(config['tokenizer_file'].format(language))
+    if not tokenizer_path.exists():
+        tokenizer = Tokenizer(WordLevel(unk_token='[UNK]'))
+        tokenizer.pre_tokenizer = Whitespace
+        trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2)
+        tokenizer.train_from_iterator(get_all_sentences(dataset, language), trainer=trainer)
+        tokenizer.save(str(tokenizer_path))
+    else:
+        tokenizer = Tokenizer.from_file(src(tokenizer_path))
+    return tokenizer
+        
